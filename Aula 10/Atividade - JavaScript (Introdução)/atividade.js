831b0cb3ccd0c84bf8b57ba9af6450098bdffc3f
DanCardoso15 <dancardoso15@hotmail.com>
2024-11-04 17:17:39 -0300

    Aula 10 - Atividade - JavaScript (Introdução)

---
+++ Aula 10/Atividade - JavaScript (Introdução)/atividade.js
@@ -0,0 +1,69 @@
+// 1. Considerando as variáveos x = 7 e y = 8, crie um código que mostre:
+
+let x = 7; y = 8;
+
+// A soma dos números.
+console.log(x + y); // 15
+
+// A diferença entre os números.
+console.log(x - y); // -1
+
+// A multiplicação dos números.
+console.log(x * y); // 56
+
+// A divisão dos números.
+console.log(x / y); // 0.875
+
+// 2. Dado o valor let x = 10, utilize operadores de atribuição para modificar e exibir o valor de x da seguinte forma:
+
+let xx = 10;
+
+// Adicione 5 a x.
+xx += 5
+console.log(xx); // 15
+
+// Subtraia 3 de x.
+xx -= 3
+console.log(xx); // 12
+
+// Multiplique x por 2.
+xx *= 2
+console.log(xx); 24
+
+// Divida x por 4.
+xx /= 4
+console.log(xx); // 6
+
+// 3. Escreva um código que tenha duas strings e verifica:
+
+// Se as duas strings são iguais (utilizando ==).
+console.log("Abacaxi" == "abacaxi"); // false
+
+// Se as duas strings são diferentes (utilizando !=).
+console.log("Uva" != "uva"); // true
+
+// 4. Escreva um código que tenha dois numeros e verifica:
+
+// Se o primeiro numero é maior que o segundo.
+console.log(5 > 6); // false
+
+// Se o primeiro numero é menor ou igual ao segundo.
+console.log(7 < 15); // true
+
+// 5. Crie um código que tenha três valores booleanos e mostre:
+
+// A conjunção de todos eles.
+console.log(5 > 1 && 9 < 15 && 5 == 5); // true
+
+// A disjunção de todos eles.
+console.log(5 > 1 || 9 < 15 || 5 == 5); // true
+
+// A negação da disjunção de todos eles.
+console.log(!(5 > 1 || 9 < 15 || 5 == 5)); // false
+
+
+
+
+
+
+
