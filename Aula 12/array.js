d687c0f9ec32e3f36087b47e96481e29b628fc20
DanCardoso15 <dancardoso15@hotmail.com>
2024-11-07 11:03:27 -0300

    Atividade - Aula 12 - Parte 2

--- Aula 12/array.js
+++ Aula 12/array.js
@@ -4,6 +4,8 @@ console.log(lang);
 console.log(lang[5]); // n
 console.log(lang[0]); // P
 
+console.log("-------------------");
+
 // vetores, array, lista, matriz
 let numero = 5;
 let numeros = [4, 8, 7, 1, 0, 6, 8, 0];
@@ -36,3 +38,18 @@ console.log("-------------------");
 for (let nome of nomes) { // "for each/para cada" é específico para percorrer cada item da lista/array
     console.log("Seja bem vindo(a),", nome); 
 }
+
+console.log("-------------------");
+
+const frutas = ["Maçã", "Laranja", "Morango", "Limão", "Pera", "Cajarana"];
+frutas.push("Ciriguela");
+frutas.pop(); // ".pop()" Remove o último elemento da lista
+console.log(frutas);
+
+console.log(frutas.includes("Caju")); // false -> ".includes()" retorna um valor boleano se o item está dentro ou fora da lista
+console.log(frutas.includes("Maçã")); // true
+console.log(frutas.indexOf("Limão")); // 3 -> ".indexOf("Limão")" retorna a posição do elemento da lista
+console.log(frutas.indexOf("Goiaba")); // -1 -> Retorna esse valor porque o elemento não está na lista
+
+frutas.splice(2, 1); // ".splice()" remove um ou mais elementos da lista onde .splice(posição do elemento, quantidade de elementos seja na mesma posição ou em demais posições)
+console.log(frutas);
